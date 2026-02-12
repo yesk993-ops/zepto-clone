380c36b16c52cbe8bd0ae10c87de2edee82c4fe3
keshav <test@test.com>
2026-02-12 19:54:09 +0530

    update

--- services/cart-service/app.js
+++ services/cart-service/app.js
@@ -1,17 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.json());
-
-let cart=[];
-
-app.post('/cart',(req,res)=>{
- cart.push(req.body);
- res.send(cart);
+app.get("/", (req,res)=> {
+ res.send("cart-service running");
 });
 
-app.get('/cart',(req,res)=>{
- res.send(cart);
-});
-
-app.listen(3001);
+app.listen(3000,()=>console.log("cart-service started"));
