83d26cc5cbc61d8a138d8b86c30834eba9ef64d9
keshav <test@test.com>
2026-02-12 17:23:05 +0530

    updating

---
+++ services/frontend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const PORT = 3000;
+
+// Serve React static files
+app.use(express.static(path.join(__dirname, "build")));
+
+// SPA fallback (Express 5 SAFE method)
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Frontend running on port ${PORT}`);
+});
