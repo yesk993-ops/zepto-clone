6d8bbcf358766e8bb7ac2b15df118906f310ac3f
keshav <test@test.com>
2026-02-12 20:10:18 +0530

    updated

--- services/frontend/server.js
+++ services/frontend/server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 8080;
 
 // Serve React static files
 app.use(express.static(path.join(__dirname, "build")));
